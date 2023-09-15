fb167f58a6139ba6a4189217497a9a61632b5cb6
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-09-14 22:35:37 -0300

    commit

---
+++ echo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Escreve na tela
+echo "Hello world"
+
+# Data atual
+date
+
+# Data atual formata dia_mes_ano
+data=`date +"%d_%m_%y"` 
+echo ${data}
+
+# Loop de 1 a 10 de 1 em 1
+for NUM in $(seq 1 1 10)
+do
+echo "Número $NUM"
+done
+
+# Declaração de funcao
+funcao(){
+  echo "echo com função"
+}
+
+# Chamando função
+funcao
